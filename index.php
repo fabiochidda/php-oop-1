3f5367ad21390930236aa1f99d954f98e95b67d2
fabiochidda <fabio.chidda99@gmail.com>
2022-04-19 02:47:00 +0200

    Aggiunta la classe

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
 
+require_once __DIR__ . "/movie.php";
+
+$fnf = new Movie(
+    "Fast and Furious",
+    "Rob Cohen",
+    "Lorem Ipsum",
+    "2001",
+);
+
+$interstellar = new Movie(
+    "Interstellar",
+    "Cristopher Nolan",
+    "Lorem Ipsum",
+    "2014",
+);
 
 ?>
 
--- movie.php
+++ movie.php
@@ -1,4 +1,20 @@
 <?php
 
+class Movie {
+
+    public $title;
+    public $director;
+    public $description;
+    public $year;
+
+    function __construct($_title, $_director, $_description, $_year){
+        $this->title=$_title;
+        $this->director=$_director;
+        $this->description=$_description;
+        $this->year=$_year;
+    }
+
+}
+
 
 ?>
